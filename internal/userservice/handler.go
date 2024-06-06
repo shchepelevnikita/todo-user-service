9d14b46fdf161bad3a9973a8d92d05ba581393f9
Nikita Shchepelev <nikita.shchepelev@7bits.it>
2024-06-06 12:11:01 +0600

    fix: imports

--- internal/userservice/handler.go
+++ internal/userservice/handler.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/shchepelevnikita/todo-user-service/pkg/response"
+	response "github.com/shchepelevnikita/todo-user-service/pkg/http/response"
 	"golang.org/x/crypto/bcrypt"
 )
 
